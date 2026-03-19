4356a20fd7786a43f2d4c0b16919d895d6700feb
RainyinSaiGon <dinhdaivu1109@gmail.com>
2026-03-19 14:24:48 +0000

    Update TCP Client, build ping pong program using command line

--- src/main.zig
+++ src/main.zig
@@ -14,34 +14,68 @@ pub fn main(init: std.process.Init) !void {
         std.log.info("arg: {s}", .{arg});
     }
 
-    if (args.len > 1 and std.mem.eql(u8, args[1],"server"))
-    {
-        std.debug.print("Hello {s}. \n", .{"world"});
-    }
-    else
-    {
-        std.debug.print("Ping {s}. \n", .{"Pong"});
-    }
-    
-
     // In order to do I/O operations need an `Io` instance. (Threaded)
     const io = init.io;
 
-    try startServer(io);
+    if (args.len > 1 and std.mem.eql(u8, args[1], "server")) {
+        try startServer(io);
+    } else {
+        try clientConnect(io);
+    }
 }
 
 pub fn startServer(io: Io) !void {
-
-    // First, we wind defined an IPaddress "127.0.0.1" with the port 1234 to our variable
     const address = try Io.net.IpAddress.parse("127.0.0.1", 1234);
-
-    // Create a socket server with TCP protocol
-    // reuse_address help us use the port immediately without waiting TIME_OUT from OS
     var server = try address.listen(io, .{ .mode = .stream, .protocol = .tcp, .reuse_address = true });
 
-    // This line will block our program until some clients connect to our server
     const stream = try server.accept(io);
 
-    // TODO: Read + Write
+    // Read from client
+    var read_buf: [1024]u8 = undefined;
+    var rd = stream.reader(io, &read_buf);
+
+    const header = try rd.interface.takeByte();
+    const data = try rd.interface.take(header);
+
+    std.debug.print("Receive from client: {s}\n", .{data});
+
+    // Write to client
+    var write_buf: [1024]u8 = undefined;
+    var wt = stream.writer(io, &write_buf);
+    try wt.interface.writeByte(@as(u8, @intCast(data.len)));
+    try wt.interface.writeAll(data);
+    try wt.interface.flush();
+
     stream.close(io);
+}
+
+pub fn clientConnect(io: Io) !void {
+    const address = try Io.net.IpAddress.parse("127.0.0.1", 1234);
+    const connection = try address.connect(io, .{ .mode = .stream, .protocol = .tcp });
+
+    // Read from stdin
+    var stdin_buf: [1024]u8 = undefined;
+    var std_reader: Io.File.Reader = .init(.stdin(), io, &stdin_buf);
+
+    const line = try std_reader.interface.takeSentinel('\n');
+    std.debug.print("Send to server: {s}\n", .{line});
+
+    // Write to server
+    var write_buf: [1024]u8 = undefined;
+    var wt = connection.writer(io, &write_buf);
+
+    try wt.interface.writeByte(@as(u8, @intCast(line.len)));
+    try wt.interface.writeAll(line);
+    try wt.interface.flush();
+
+    // Read from server
+    var read_buf: [1024]u8 = undefined;
+    var rd = connection.reader(io, &read_buf);
+
+    const header = try rd.interface.takeByte();
+    const data = try rd.interface.take(header);
+
+    std.debug.print("Receive from server: {s}\n", .{data});
+
+    connection.close(io);
 }
